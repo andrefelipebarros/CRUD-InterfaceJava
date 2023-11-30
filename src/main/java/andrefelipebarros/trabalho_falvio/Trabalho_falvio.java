5b5cc582491eeb4e96b6426b159792092f58d44e
André Felipe de Barros A. N <afbarrosan@gmail.com>
2023-11-30 00:46:41 -0300

    Adding a project

---
+++ src/main/java/andrefelipebarros/trabalho_falvio/Trabalho_falvio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package andrefelipebarros.trabalho_falvio;
+
+/**
+ *
+ * @author afbar
+ */
+public class Trabalho_falvio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
